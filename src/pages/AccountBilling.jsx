944ffd50724204a4701e17f971fd8c35476edda2
harshpatelsyndell <harshpatel.syndell@gmail.com>
2024-01-11 16:13:28 +0530

    sidebar done

--- src/pages/AccountBilling.jsx
+++ src/pages/AccountBilling.jsx
@@ -3,6 +3,10 @@
 import { useEffect, useState } from "react";
 import Navigation from "../components/Navigation";
 import Sidebar from "../components/Sidebar";
+import transaction from "../assets/transaction.png";
+import payment from "../assets/payment.png";
+import billing from "../assets/billing.png";
+import subplan from "../assets/sub&plan.png";
 
 export default function AccountBilling() {
   const [showSidebar, setShowSidebar] = useState(window.innerWidth > 768);
@@ -34,6 +38,41 @@ export default function AccountBilling() {
           logo={false}
           menubtn={true}
         />
+        <BillingSec />
+      </div>
+    </div>
+  );
+}
+
+function BillingSec() {
+  return (
+    <div className="bg-white1 h-screen p-5">
+      <div className="flex space-x-3 text-xs">
+        <a href="#">Deshboard</a>
+        <p>&gt;</p>
+        <a href="#">Account & Billing</a>
+        <p>&gt;</p>
+        <a href="#" className="text-orange">
+          Overview
+        </a>
+      </div>
+      <div className="flex items-center my-5 text-sm space-x-5">
+        <div className="flex items-center text-orange space-x-2">
+          <img className="bg-orange p-1 rounded" src={subplan} alt="plan" />
+          <p>Subscription & add-ons</p>
+        </div>
+        <div className="flex items-center space-x-2">
+          <img className="p-1 rounded" src={billing} alt="plan" />
+          <p>Billing information</p>
+        </div>
+        <div className="flex items-center space-x-2">
+          <img className="p-1 rounded" src={transaction} alt="plan" />
+          <p>Transactions</p>
+        </div>
+        <div className="flex items-center space-x-2">
+          <img className="p-1 rounded" src={payment} alt="plan" />
+          <p>Payment Methods</p>
+        </div>
       </div>
     </div>
   );
